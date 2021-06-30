5a011b077522d5179b230f8265b5b51ae6074524
Rogério Lopes <44215142+rogeriolopes16@users.noreply.github.com>
2021-06-30 09:27:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,18 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 template = Jinja2Templates(directory="template")
 
-
+# cria rota principal da aplicação
 @app.get("/")
 async def index(request: Request):
     return template.TemplateResponse("index.html", {"request": request})
 
+# rota de envio de json para Rabbit
 @app.get("/entrada/", response_class=HTMLResponse)
 async def entrada(request: Request, id: str, menssagem: str):
     response = sendRabbitMQ({"id": id, "notification": menssagem})
     return template.TemplateResponse("index.html", {"request": request, "response": response})
 
+# rota para pesquisar se existe a notificação no S3
 @app.get("/recuperar/", response_class=HTMLResponse)
 async def recuperar(request: Request, idresp: str):
     responseS3 = requestS3(idresp)
